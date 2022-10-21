1ae5954eb2ef41a6fcc7cf41d690e3ee5aa1a311
Soumaila DIAKITE <soum.diakite@outlook.com>
2022-10-21 19:46:10 +0200

    Initiation du projet

---
+++ src/main/java/com/dsoumaila/tdd_double_loop/Account.java
@@ -0,0 +1,20 @@
+package com.dsoumaila.tdd_double_loop;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Account {
+    public Account() {
+
+    }
+
+    public String printStatement() {
+        return null;
+    }
+
+    public void deposit(BigDecimal bigDecimal, LocalDate of) {
+    }
+
+    public void withdraw(BigDecimal bigDecimal, LocalDate of) {
+    }
+}
